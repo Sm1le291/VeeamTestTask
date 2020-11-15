aad9873ef67538931a80f36efd75edb141e4bcbc
Igor Nesterov <fasteat@outlook.com>
2020-11-15 22:36:41 +0100

    add one more test

--- VeeamArchiveTool.Services.Tests/FileProcessorTests.cs
+++ VeeamArchiveTool.Services.Tests/FileProcessorTests.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using Moq;
@@ -5,6 +6,7 @@ using Moq;
 using System;
 using System.IO;
 
+using VeeamArchiveTool.Common;
 using VeeamArchiveTool.Common.Abstractions;
 
 namespace VeeamArchiveTool.Services.Tests
@@ -13,6 +15,10 @@ namespace VeeamArchiveTool.Services.Tests
     public class FileProcessorTests
     {
         private string _testFilePath;
+
+        private const int FILE_SIZE_MB = 2048;
+
+        private const int CHUNK_SIZE_BYTE = 1024 * 1024 * 5;
 
         [TestInitialize]
         public void SetUp()
@@ -26,7 +32,7 @@ namespace VeeamArchiveTool.Services.Tests
             Random rng = new Random();
             using (FileStream stream = File.OpenWrite(_testFilePath))
             {
-                for (int i = 0; i < 2048 * 128; i++)
+                for (int i = 0; i < FILE_SIZE_MB * 128; i++)
                 {
                     rng.NextBytes(data);
                     stream.Write(data, 0, data.Length);
@@ -35,14 +41,26 @@ namespace VeeamArchiveTool.Services.Tests
         }
 
         [TestMethod]
-        public void TestMethod1()
+        [TestCategory("Integration")]
+        public void GetChunks_ReturnChunksFromInputFile_ShouldReturnNumberOfChunksEquiualentFileLengthDivideToChunkSize()
         {
             var executionContextMock = new Mock<IExecutionContext>();
+            executionContextMock.Setup(x => x.ChunkSize).Returns(CHUNK_SIZE_BYTE);
+            executionContextMock.Setup(x => x.ProcessDirection).Returns(Common.Models.ProcessDirection.Compress);
+            var exceptionHandlerMock = new Mock<IExceptionHandler>();
+            var loggerMock = new Mock<ILogger<FileProcessor>>();
+            var threadPool = new ThreadPool(new Mock<ILogger<ThreadPool>>().Object);
             FileInfo fileInfo = new FileInfo(_testFilePath);
+            var blockingCollection = new BlockingQueue<DomainModels.Chunk>();
 
-            //FileProcessor fileProcessor = new FileProcessor();
-            //fileProcessor.GetChunks()
-            //var sd = "sdf";
+            var expectedCount = 410; // FILE_SIZE_MB / CHUNK_SIZE_BYTE and round to upper
+
+            FileProcessor fileProcessor = new FileProcessor(executionContextMock.Object, threadPool, loggerMock.Object, exceptionHandlerMock.Object);
+            fileProcessor.GetChunks(fileInfo, blockingCollection);
+            var actualCount = blockingCollection.Count;
+            blockingCollection = null;
+
+            Assert.AreEqual(expectedCount, actualCount);
         }
 
         [TestCleanup]
